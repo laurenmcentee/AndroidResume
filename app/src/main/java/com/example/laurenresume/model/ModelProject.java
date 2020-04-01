5710c4df78c21109fea4fbd6e3dcf0cc878f7791
Lauren McEntee <Lauren_McEntee@insideranken.org>
2020-04-01 13:15:55 -0500

    Added model package and model classes

---
+++ app/src/main/java/com/example/laurenresume/model/ModelProject.java
@@ -0,0 +1,50 @@
+package com.example.laurenresume.model;
+
+public class ModelProject {
+
+    public String projectName;
+    public String projectType;
+    public String projectDescription;
+    public String projectImgePath;
+    public String projectLandingPage;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectType() {
+        return projectType;
+    }
+
+    public void setProjectType(String projectType) {
+        this.projectType = projectType;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public String getProjectImgePath() {
+        return projectImgePath;
+    }
+
+    public void setProjectImgePath(String projectImgePath) {
+        this.projectImgePath = projectImgePath;
+    }
+
+    public String getProjectLandingPage() {
+        return projectLandingPage;
+    }
+
+    public void setProjectLandingPage(String projectLandingPage) {
+        this.projectLandingPage = projectLandingPage;
+    }
+}
